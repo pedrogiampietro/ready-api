8fcf08a768e78febfa23154f171bfa8e263c4141
Pedro Giampietro <pedrogiampietro@hotmail.com>
2024-08-21 11:55:27 -0300

    update seed

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -3,35 +3,54 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 async function main() {
-  // Deleta todos os registros de Plan antes de recriá-los
-  await prisma.plan.deleteMany();
-
-  // Cria o plano FREE
-  const freePlan = await prisma.plan.create({
-    data: {
-      id: "free-plan-id", // ID opcionalmente predefinido, pode ser removido se preferir um UUID gerado automaticamente
-      name: "FREE",
-      features: [
-        "Acesso limitado a funcionalidades",
-        "Visualização de viagens",
-      ],
+  // Verifica se o plano FREE já existe
+  const existingFreePlan = await prisma.plan.findUnique({
+    where: {
+      id: "free-plan-id",
     },
   });
 
-  // Cria o plano PRO
-  const proPlan = await prisma.plan.create({
-    data: {
-      id: "pro-plan-id", // ID opcionalmente predefinido, pode ser removido se preferir um UUID gerado automaticamente
-      name: "PRO",
-      features: [
-        "Acesso completo a todas as funcionalidades",
-        "Criação ilimitada de viagens",
-        "Suporte prioritário",
-      ],
+  // Se o plano FREE não existir, ele será criado
+  if (!existingFreePlan) {
+    const freePlan = await prisma.plan.create({
+      data: {
+        id: "free-plan-id", // ID opcionalmente predefinido, pode ser removido se preferir um UUID gerado automaticamente
+        name: "FREE",
+        features: [
+          "Acesso limitado a funcionalidades",
+          "Visualização de viagens",
+        ],
+      },
+    });
+    console.log("Plano FREE criado:", freePlan);
+  } else {
+    console.log("Plano FREE já existe, pulando criação.");
+  }
+
+  // Verifica se o plano PRO já existe
+  const existingProPlan = await prisma.plan.findUnique({
+    where: {
+      id: "pro-plan-id",
     },
   });
 
-  console.log({ freePlan, proPlan });
+  // Se o plano PRO não existir, ele será criado
+  if (!existingProPlan) {
+    const proPlan = await prisma.plan.create({
+      data: {
+        id: "pro-plan-id", // ID opcionalmente predefinido, pode ser removido se preferir um UUID gerado automaticamente
+        name: "PRO",
+        features: [
+          "Acesso completo a todas as funcionalidades",
+          "Criação ilimitada de viagens",
+          "Suporte prioritário",
+        ],
+      },
+    });
+    console.log("Plano PRO criado:", proPlan);
+  } else {
+    console.log("Plano PRO já existe, pulando criação.");
+  }
 }
 
 main()
